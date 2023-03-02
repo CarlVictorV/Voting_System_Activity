2a84661b71d95935f00dcf599db859a1bf17452f
CarlVictorV <111956628+CarlVictorV@users.noreply.github.com>
2023-03-02 08:41:12 +0800

    Delete So far directory

--- So far/So far/DistrictRepresentatives.java
+++
@@ -1,4 +0,0 @@
-
-public class DistrictRepresentatives extends Candidates{
-
-}
--- So far/So far/Officer.java
+++
@@ -1,8 +0,0 @@
-
-public class Officer extends User {
-
-	public Officer(String string, String string2, String string3) {
-        super(string, string2, string3);
-	}
-
-}
