b61e236c6c59478727b9cba3c66fb01980e6b00a
CarlVictorV <111956628+CarlVictorV@users.noreply.github.com>
2023-03-02 12:05:55 +0800

    Update Voter.java

--- for vs/src/Voter.java
+++ for vs/src/Voter.java
@@ -1,12 +1,19 @@
 public class Voter extends User {
 	private boolean voted = false;
-	
+
 	public Voter(String string, String string2, String string3) {
 		super(string, string2, string3);
 	}
+
+	public Voter(String string, String string2, String string3, boolean voted) {
+		super(string, string2, string3);
+		this.voted = voted;
+	}
+
 	public boolean isVoted() {
 		return voted;
 	}
+
 	public void setVoted(boolean voted) {
 		this.voted = voted;
 	}
